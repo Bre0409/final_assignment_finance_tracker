7ffe80083b46bf6d040813a3d02043faa3ae7ea3
Bre0409 <brendanboland@hotmail.co.uk>
2026-01-26 22:05:42 +0000

    final fix hopefully

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,37 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(
+        required=False,
+        widget=forms.EmailInput(attrs={"class": "form-control", "placeholder": "you@example.com"}),
+    )
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "form-control", "placeholder": "Choose a username"}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Apply Bootstrap to password fields created by UserCreationForm
+        self.fields["password1"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Create a password",
+        })
+        self.fields["password2"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Confirm your password",
+        })
+
+        # Optional: make labels nicer
+        self.fields["password1"].label = "Password"
+        self.fields["password2"].label = "Confirm password"
+
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
