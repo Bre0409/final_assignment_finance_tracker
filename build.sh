c9a39421e5e4b3fd67d5a2d14ffa6d6d87e17ae4
Bre0409 <brendanboland@hotmail.co.uk>
2026-01-26 17:45:05 +0000

    Make build.sh executable + add log markers

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "=== BUILD.SH STARTED ==="
+python --version
+pwd
+ls -la
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+echo "=== RUNNING CREATE_SUPERUSER ==="
 python create_superuser.py
+echo "=== CREATE_SUPERUSER FINISHED ==="
+
+echo "=== BUILD.SH FINISHED ==="
